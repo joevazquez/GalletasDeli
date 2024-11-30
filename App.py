18e9484d3f3ecd7a1c99a343358576629286caf2
Joel Vázquez Anaya <54601149+joevazquez@users.noreply.github.com>
2024-11-29 20:41:02 -0600

    Cambios en la aplicación

--- App.py
+++ App.py
@@ -1101,4 +1101,7 @@ def logout():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
